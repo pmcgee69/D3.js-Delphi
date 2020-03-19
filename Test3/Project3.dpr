67c58cf889ab6f78d4920071ff82b38000e6621e
pmcgee69 <paulmcgee1969@gmail.com>
2020-03-19 11:27:21 +0800

    Add Commonj directory

--- Test3/Project3.dpr
+++ Test3/Project3.dpr
@@ -4,9 +4,9 @@ uses
   Vcl.Forms,
   Unit3 in 'Unit3.pas' {Form1},
   D3_Test_procs in 'D3_Test_procs.pas',
-  WebUtils in '..\WebUtils.pas',
-  UWebBrowserWrapper in '..\UWebBrowserWrapper.pas',
-  MSHTML_TLB in '..\MSHTML_TLB.pas';
+  WebUtils           in '..\Common\WebUtils.pas',
+  UWebBrowserWrapper in '..\Common\UWebBrowserWrapper.pas',
+  MSHTML_TLB         in '..\Common\MSHTML_TLB.pas';
 
 {$R *.res}
 
